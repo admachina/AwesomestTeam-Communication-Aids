cd2ddf47a3216f7ebe17ad8b197d9c52558d0bf2
Andrew Machina <admachina@gmail.com>
2011-11-14 17:30:41 -0500

    prelim alg implementation

--- Alg.java
+++ Alg.java
@@ -1,6 +1,28 @@
-import java.string.*;
-
 public class Alg
 {
-	String choose(int choice, String newValues[]);
+	private MyTree tree;
+	private int numChoices;
+
+	public Alg(int choices)
+	{
+		numChoices = choices;
+		tree = new MyTree(choices);
+	}
+
+	public String choose(int choice, String newValues[]){
+		tree = tree.next(choice);
+
+		String printVal = tree.printValue();
+
+		if(tree.isLeaf())
+		{
+			tree = tree.root();
+		}
+
+		for(int i=0; i<numChoices; i++)
+		{
+			newValues[i] = tree.next(i).display();
+		}
+		
+	}
 }
