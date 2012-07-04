ac8baf2e5f2fe04dec1b635425073b4102cc2470
Andrew Machina <admachina@gmail.com>
2012-07-04 11:59:00 -0400

    added objc code for tree (currently missing clone)

---
+++ iOSCode/Communication_Aid/Classes/SelectionTree.h
@@ -0,0 +1,36 @@
+//
+//  SelectionTree.h
+//  Communication_Aid
+//
+//  Created by admachin on 7/3/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SelectionTree : NSObject {
+    NSMutableArray* _branches;
+    
+    NSString* _printValue;
+    NSString* _displayValue;
+}
+
+//Constructors
+-(SelectionTree*) init;
+-(SelectionTree*) init: (NSString*) displayValue;
+-(SelectionTree*) init: (NSString*) displayValue: (NSString*) printValue;
+
+-(SelectionTree*) clone;
+
+// Accessors
+-(SelectionTree*) next: (int) branch;
+
+-(NSString*) displayValue;
+-(NSString*) printValue;
+-(Boolean) isLeaf;
+
+//Mutators
+-(void) addNode: (SelectionTree*) addedTree: (int) location;
+-(void) addNode: (SelectionTree*) addedTree;
+
+@end
