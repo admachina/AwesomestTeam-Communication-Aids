30bd1600f281c6e5e6fb94fde4f17d48d058ade1
Victoria Lee <Vicky@Victor-Tongs-MacBook.local>
2013-03-16 10:29:12 -0400

    options tree class

---
+++ iOSCode/Communication_Aid/Classes/OptionsTree.h
@@ -0,0 +1,13 @@
+//
+//  OptionsTree.h
+//  Communication_Aid
+//
+//  Created by Victoria Lee on 13-03-15.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OptionsTree : NSObject
+
+@end
---
+++ iOSCode/Communication_Aid/Classes/OptionsTree.m
@@ -0,0 +1,13 @@
+//
+//  OptionsTree.m
+//  Communication_Aid
+//
+//  Created by Victoria Lee on 13-03-15.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "OptionsTree.h"
+
+@implementation OptionsTree
+
+@end
