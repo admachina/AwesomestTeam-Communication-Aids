d372b1822d5057a34d12ffa0b7d0d9318abab853
Victoria Lee <Vicky@Victor-Tongs-MacBook.local>
2013-03-20 23:23:35 -0400

    have code that "should" implement options trees and email but I can't build the fucking project... something in my checkouts is broken within the calibration view controller

---
+++ iOSCode/Communication_Aid/Classes/EmailViewController.h
@@ -0,0 +1,13 @@
+//
+//  EmailViewController.h
+//  Communication_Aid
+//
+//  Created by Victoria Lee on 13-03-20.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EmailViewController : NSObject
+
+@end
---
+++ iOSCode/Communication_Aid/Classes/EmailViewController.m
@@ -0,0 +1,13 @@
+//
+//  EmailViewController.m
+//  Communication_Aid
+//
+//  Created by Victoria Lee on 13-03-20.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "EmailViewController.h"
+
+@implementation EmailViewController
+
+@end
